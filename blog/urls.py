70a21eb12b73a2c59c52a35d47863ea23649e85d
guzpj <guzpj69@gmail.com>
2016-10-12 12:13:59 +0200

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.post_list),
+        url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+        # ^ el principio.
+        # post/ después del comienzo, la dirección URL debe contener la palabra post y /.
+        # (?P<pk>[0-9]+) Django llevará todo lo que coloques aquí y lo transferirá
+        # a una vista como una variable llamada pk (primary key). [0-9], indica que
+        # sólo puede ser un número, no una letra (todo debería estar entre 0 y 9).
+        # + significa que tiene que haber uno o más dígitos.
+        #/ necesario para que la url termine en /.
+        #$ indica el final.
     ]
